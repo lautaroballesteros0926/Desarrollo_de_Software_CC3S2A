2f27f514ecf64a512b788679e747ba58699a772e
lautaroballesteros0926 <lautaroballesteros26@gmail.com>
2024-09-04 10:33:41 -0500

    Arreglando configuaraciones

--- devops-practice_Actividad1/src/app.js
+++ devops-practice_Actividad1/src/app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    res.send('Hello, World!');
 });
 
 module.exports = app;
+
+
+if (require.main === module) {
+    const port = process.env.PORT || 0; 
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
